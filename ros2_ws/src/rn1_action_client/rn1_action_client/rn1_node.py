6aaba726018f890e36abd6819662e99c79870d6b
unboagable <unboagable@gmail.com>
2025-03-09 13:20:16 -0400

    Clean up code

--- ros2_ws/src/rn1_action_client/rn1_action_client/rn1_node.py
+++ ros2_ws/src/rn1_action_client/rn1_action_client/rn1_node.py
@@ -1,24 +1,15 @@
 import json
 import os
+import time
 import rclpy
 from rclpy.node import Node
 import paho.mqtt.client as mqtt
 from example_interfaces.action import Fibonacci
 from rclpy.action import ActionClient
 
-
 MQTT_BROKER = os.getenv("MQTT_BROKER", "mqtt")
-
-
-#MQTT_BROKER = "project_root-mqtt-1"
 MQTT_TOPIC = "missions"
-
-import time
-import paho.mqtt.client as mqtt
-
 MAX_RETRIES = 5
-
-
 
 class RN1Client(Node):
     def __init__(self):
@@ -31,10 +22,10 @@ class RN1Client(Node):
                 self.mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
                 self.mqtt_client.on_message = self.on_mqtt_message
                 self.mqtt_client.connect(MQTT_BROKER, 1883, keepalive=300)
-                print("MQTT Connection Successful!")
+                self.get_logger().info("MQTT Connection Successful!")
                 break
             except ConnectionRefusedError:
-                print(f"MQTT Connection failed, retrying ({attempt+1}/{MAX_RETRIES})...")
+                self.get_logger().warning(f"MQTT Connection failed, retrying ({attempt+1}/{MAX_RETRIES})...")
                 time.sleep(2)
         
         self.mqtt_client.subscribe(MQTT_TOPIC)
